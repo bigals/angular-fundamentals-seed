96d01ed05c89ec91cc92b411a1e76da270a21f7b
bigals <alexander.wil.mclean@gmail.com>
2018-02-18 02:20:10 -0500

    end of form validation and error section

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -14,14 +14,24 @@ import { Baggage } from '../../models/baggage.interface';
                 <input
                     type="text"
                     name="fullname"
+                    required
+                    #fullname="ngModel"
                     [ngModel]="detail?.fullname">
+                   <div *ngIf="fullname.errors?.required &&fullname.dirty" class="error">
+                        Passenger name is required
+                   </div>
             </div>
             <div>
                 Passenger ID:
                 <input
                     type="number"
+                    required
                     name="id"
+                    #id="ngModel"
                     [ngModel]="detail?.id">
+                    <div *ngIf="id.errors?.required && id.dirty" class="error">
+                        Passenger ID is required
+                   </div>
             </div>
             <div>
                 <label>
@@ -53,7 +63,9 @@ import { Baggage } from '../../models/baggage.interface';
                     </option>
                 </select>
             </div>
-            {{ form.value | json }}
+            <div>{{ form.value | json }}</div>
+            <div>Valid{{ form.valid | json }}</div>
+            <div>InValid{{ form.invalid | json }}</div>
         </form>
     `
 })
