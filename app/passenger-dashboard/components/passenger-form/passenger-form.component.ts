091ceeaf7724e263401c111d4cedadfc1a377558
bigals <alexander.wil.mclean@gmail.com>
2018-02-18 01:55:05 -0500

    end of binding to radio buttons section

--- app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
+++ app/passenger-dashboard/components/passenger-form/passenger-form.component.ts
@@ -22,6 +22,33 @@ import { Passenger } from '../../models/passenger.interface';
                     name="id"
                     [ngModel]="detail?.id">
             </div>
+            <div>
+                <label>
+                    <input
+                        type="radio"
+                        [value]="true"
+                        name="checkedIn"
+                        [ngModel]="detail?.checkedIn"
+                        (ngModelChange)="toggleCheckIn($event)">
+                    Yes
+                </label>
+                <label>
+                    <input
+                        type="radio"
+                        [value]="false"
+                        name="checkedIn"
+                        [ngModel]="detail?.checkedIn"
+                        (ngModelChange)="toggleCheckIn($event)">
+                    No
+                </label>
+            </div>
+            <div *ngIf="form.value.checkedIn">
+                Check in Date:
+                <input
+                    type="number"
+                    name="checkInDate"
+                    [ngModel]="detail?.checkInDate">
+            </div>
             {{ form.value | json }}
         </form>
     `
@@ -33,4 +60,10 @@ export class PassengerFormComponent implements OnInit {
     constructor() {}
 
     ngOnInit() { }
+
+    toggleCheckIn(checkedIn: boolean) {
+        if (checkedIn) {
+            this.detail.checkInDate = Date.now();
+        }
+    }
 }
