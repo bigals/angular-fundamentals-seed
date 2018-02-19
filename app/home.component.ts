6d74ca195119400046d595c20a1fa5337c969f19
bigals <alexander.wil.mclean@gmail.com>
2018-02-18 20:21:27 -0500

    start of wildcard routes lesson

---
+++ app/home.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    template: `
+        <div>
+            Airline passenger App!
+        </div>
+    `
+})
+
+export class HomeComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
